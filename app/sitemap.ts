d91cf1c1a30573b5f9e9b7910cf8281d3bb8a0e2
khan <ravikumar02860@gmail.com>
2026-05-21 11:03:13 +0530

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://invoigen.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://invoigen.vercel.app/en",
+      lastModified: new Date(),
+    },
+  ];
+}
