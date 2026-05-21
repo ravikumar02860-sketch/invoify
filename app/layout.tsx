43f4ff8d77d236140545a447d5d12cee6d2412cf
khan <ravikumar02860@gmail.com>
2026-05-21 10:58:01 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,10 @@
+import { ReactNode } from "react";
+import "@/app/globals.css";
+
+type Props = {
+  children: ReactNode;
+};
+
 export default function RootLayout({ children }: Props) {
   return (
     <html>
